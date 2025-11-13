0be404df628ef15ee73c6c653a77b4e8bd636465
Felipe-Santos <felipesantosswengineer@gmail.com>
2025-11-12 23:37:43 -0300

    v1.1 - with local download in railway

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ import pdfplumber
 import nltk
 import io
 
-
 NLTK_DATA_DIR = os.path.join(os.path.dirname(__file__), "nltk_data")
 os.makedirs(NLTK_DATA_DIR, exist_ok=True)
-
-nltk.data.path.append(NLTK_DATA_DIR)
-
 nltk.download("punkt", download_dir=NLTK_DATA_DIR)
 nltk.download("stopwords", download_dir=NLTK_DATA_DIR)
 nltk.download("wordnet", download_dir=NLTK_DATA_DIR)
+nltk.data.path.insert(0, NLTK_DATA_DIR)
+
 
 # Config da API e do HF #
 load_dotenv()
